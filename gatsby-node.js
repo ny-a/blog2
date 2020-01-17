e871f7ec3e153bb8feec22719ca94e8bce3347e2
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-17 15:56:31 +0900

    Add pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,12 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const {createFilePath} = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.createPages = async ({graphql, actions}) => {
+    const {createPage} = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
-  const result = await graphql(
-    `
+    const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
+    const result = await graphql(
+        `
       {
         allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
@@ -25,40 +25,57 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     `
-  )
+    )
 
-  if (result.errors) {
-    throw result.errors
-  }
+    if (result.errors) {
+        throw result.errors
+    }
 
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+    posts.forEach((post, index) => {
+        const previous = index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
 
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
+        createPage({
+            path: post.node.fields.slug,
+            component: blogPost,
+            context: {
+                slug: post.node.fields.slug,
+                previous,
+                next,
+            },
+        })
     })
-  })
+
+    // Create blog post list pages
+    const postsPerPage = 10;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({length: numPages}).forEach((_, i) => {
+        createPage({
+            path: i === 0 ? `/` : `/${i + 1}`,
+            component: path.resolve('./src/templates/index.tsx'),
+            context: {
+                limit: postsPerPage,
+                skip: i * postsPerPage,
+                numPages,
+                currentPage: i + 1
+            },
+        });
+    });
 }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+exports.onCreateNode = ({node, actions, getNode}) => {
+    const {createNodeField} = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
+    if (node.internal.type === `MarkdownRemark`) {
+        const value = createFilePath({node, getNode})
+        createNodeField({
+            name: `slug`,
+            node,
+            value,
+        })
+    }
 }
---
+++ src/templates/index.tsx
@@ -0,0 +1,157 @@
+import React from 'react'
+import {graphql, Link} from 'gatsby'
+
+import Bio from '../components/bio'
+import Layout, {LocationType} from '../components/layout'
+import SEO from '../components/seo'
+import {rhythm} from '../utils/typography'
+
+type BlogIndexProps = {
+    data: PageQueryType,
+    location: LocationType,
+    pageContext: {
+        limit: number,
+        skip: number,
+        numPages: number,
+        currentPage: number,
+    }
+}
+
+class BlogIndex extends React.Component<BlogIndexProps> {
+    render() {
+        const {data} = this.props
+        const siteTitle = data.site.siteMetadata.title
+        const posts = data.allMarkdownRemark.edges
+        const {currentPage, numPages} = this.props.pageContext
+        const isFirst = currentPage === 1
+        const isLast = currentPage === numPages
+        const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+        const nextPage = (currentPage + 1).toString()
+
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO title="All posts"/>
+                <Bio/>
+                {posts.map(({node}) => {
+                    const title = node.frontmatter.title || node.fields.slug
+                    return (
+                        <article key={node.fields.slug}>
+                            <header>
+                                <h3
+                                    style={{
+                                        marginBottom: rhythm(1 / 4),
+                                    }}
+                                >
+                                    <Link style={{boxShadow: 'none'}} to={node.fields.slug}>
+                                        {title}
+                                    </Link>
+                                </h3>
+                                <small>{node.frontmatter.date}</small>
+                            </header>
+                            <section>
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: node.excerpt,
+                                    }}
+                                />
+                            </section>
+                        </article>
+                    )
+                })}
+                <ul
+                    style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        listStyle: 'none',
+                        padding: 0,
+                    }}
+                >
+                    {!isFirst && (
+                        <Link to={prevPage} rel="prev">
+                            ← Previous Page
+                        </Link>
+                    )}
+                    {Array.from({length: numPages}, (_, i) => (
+                        <li
+                            key={`pagination-number${i + 1}`}
+                            style={{
+                                margin: 0,
+                            }}
+                        >
+                            <Link
+                                to={`/${i === 0 ? '' : i + 1}`}
+                                style={{
+                                    padding: rhythm(1 / 4),
+                                    textDecoration: 'none',
+                                    color: i + 1 === currentPage ? '#ffffff' : '',
+                                    background: i + 1 === currentPage ? '#007acc' : '',
+                                }}
+                            >
+                                {i + 1}
+                            </Link>
+                        </li>
+                    ))}
+                    {!isLast && (
+                        <Link to={nextPage} rel="next">
+                            Next Page →
+                        </Link>
+                    )}
+                </ul>
+            </Layout>
+        )
+    }
+}
+
+export default BlogIndex
+
+type PageQueryType = {
+    site: {
+        siteMetadata: {
+            title: string,
+        }
+    },
+    allMarkdownRemark: {
+        edges: [{
+            node: {
+                excerpt: string,
+                fields: {
+                    slug: string,
+                },
+                frontmatter: {
+                    date: string,
+                    title: string,
+                }
+            }
+        }]
+    }
+}
+
+export const pageQuery = graphql`
+  query blogPageQuery($skip: Int!, $limit: Int!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 50)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
