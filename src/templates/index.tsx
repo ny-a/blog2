d97fd97618c510c8a132734401c55b9562464df6
ny-a <58758248+ny-a@users.noreply.github.com>
2020-01-25 00:15:09 +0900

    Change pagination behavior

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -7,125 +7,125 @@ import SEO from '../components/seo'
 import {rhythm} from '../utils/typography'
 
 type BlogIndexProps = {
-    data: PageQueryType,
-    location: LocationType,
-    pageContext: {
-        limit: number,
-        skip: number,
-        numPages: number,
-        currentPage: number,
-    }
+  data: PageQueryType,
+  location: LocationType,
+  pageContext: {
+    limit: number,
+    skip: number,
+    numPages: number,
+    currentPage: number,
+  }
 }
 
 class BlogIndex extends React.Component<BlogIndexProps> {
-    render() {
-        const {data} = this.props
-        const siteTitle = data.site.siteMetadata.title
-        const posts = data.allMarkdownRemark.edges
-        const {currentPage, numPages} = this.props.pageContext
-        const isFirst = currentPage === 1
-        const isLast = currentPage === numPages
-        const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
-        const nextPage = (currentPage + 1).toString()
+  render() {
+    const {data} = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+    const {currentPage, numPages} = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isOldest = currentPage === numPages
+    const newerPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const olderPage = (currentPage + 1).toString()
 
-        return (
-            <Layout location={this.props.location} title={siteTitle}>
-                <SEO title="All posts"/>
-                <Bio/>
-                {posts.map(({node}) => {
-                    const title = node.frontmatter.title || node.fields.slug
-                    return (
-                        <article key={node.fields.slug}>
-                            <header>
-                                <h3
-                                    style={{
-                                        marginBottom: rhythm(1 / 4),
-                                    }}
-                                >
-                                    <Link style={{boxShadow: 'none'}} to={node.fields.slug}>
-                                        {title}
-                                    </Link>
-                                </h3>
-                                <small>{node.frontmatter.date}</small>
-                            </header>
-                            <section>
-                                <p
-                                    dangerouslySetInnerHTML={{
-                                        __html: node.excerpt,
-                                    }}
-                                />
-                            </section>
-                        </article>
-                    )
-                })}
-                <ul
-                    style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        listStyle: 'none',
-                        padding: 0,
-                    }}
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts"/>
+        <Bio/>
+        {posts.map(({node}) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
                 >
-                    {!isFirst && (
-                        <Link to={prevPage} rel="prev">
-                            ← Previous Page
-                        </Link>
-                    )}
-                    {Array.from({length: numPages}, (_, i) => (
-                        <li
-                            key={`pagination-number${i + 1}`}
-                            style={{
-                                margin: 0,
-                            }}
-                        >
-                            <Link
-                                to={`/${i === 0 ? '' : i + 1}`}
-                                style={{
-                                    padding: rhythm(1 / 4),
-                                    textDecoration: 'none',
-                                    color: i + 1 === currentPage ? '#ffffff' : '',
-                                    background: i + 1 === currentPage ? '#007acc' : '',
-                                }}
-                            >
-                                {i + 1}
-                            </Link>
-                        </li>
-                    ))}
-                    {!isLast && (
-                        <Link to={nextPage} rel="next">
-                            Next Page →
-                        </Link>
-                    )}
-                </ul>
-            </Layout>
-        )
-    }
+                  <Link style={{boxShadow: 'none'}} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })}
+        <ul
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            listStyle: 'none',
+            padding: 0,
+          }}
+        >
+          {!isOldest && (
+            <Link to={olderPage} rel="next">
+              ← Older Page
+            </Link>
+          )}
+          {Array.from({length: numPages}, (_, i) => (
+            <li
+              key={`pagination-number${numPages - i}`}
+              style={{
+                margin: 0,
+              }}
+            >
+              <Link
+                to={`/${numPages - i === 1 ? '' : numPages - i}`}
+                style={{
+                  padding: rhythm(1 / 4),
+                  textDecoration: 'none',
+                  color: numPages - i === currentPage ? '#ffffff' : '',
+                  background: numPages - i === currentPage ? '#007acc' : '',
+                }}
+              >
+                {numPages - i}
+              </Link>
+            </li>
+          ))}
+          {!isFirst && (
+            <Link to={newerPage} rel="prev">
+              Newer Page →
+            </Link>
+          )}
+        </ul>
+      </Layout>
+    )
+  }
 }
 
 export default BlogIndex
 
 type PageQueryType = {
-    site: {
-        siteMetadata: {
-            title: string,
-        }
-    },
-    allMarkdownRemark: {
-        edges: [{
-            node: {
-                excerpt: string,
-                fields: {
-                    slug: string,
-                },
-                frontmatter: {
-                    date: string,
-                    title: string,
-                }
-            }
-        }]
+  site: {
+    siteMetadata: {
+      title: string,
     }
+  },
+  allMarkdownRemark: {
+    edges: [{
+      node: {
+        excerpt: string,
+        fields: {
+          slug: string,
+        },
+        frontmatter: {
+          date: string,
+          title: string,
+        }
+      }
+    }]
+  }
 }
 
 export const pageQuery = graphql`
